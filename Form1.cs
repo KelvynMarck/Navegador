8cdee3400acb99c2e84ebae59321dfa143b023b5
KelvynMarck <95387095+kelvyn32@users.noreply.github.com>
2022-03-14 01:06:42 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            navegador.GoBack();
+        }
+
+        private void btnAdelante_Click(object sender, EventArgs e)
+        {
+            navegador.GoForward();
+        }
+
+        private void bntAtualizar_Click(object sender, EventArgs e)
+        {
+            navegador.Refresh();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            navegador.Stop();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            navegador.GoHome();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            navegador.Navigate(URLNavega.Text);
+        }
+    }
+
+
+}
